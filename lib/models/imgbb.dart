6b5cf6552956585838ed6707d759e9baf3a1cccb
blackEye <69810869+xrwy@users.noreply.github.com>
2024-01-01 00:53:07 +0300

    Update imgbb.dart

--- lib/models/imgbb.dart
+++ lib/models/imgbb.dart
@@ -1,6 +1,6 @@
 class ImgBb {
   final String _apiUrl = "https://api.imgbb.com/1/upload";
-  final String _apiKey = "54c71b7361b4c5c6d495d474cfe9b378";
+  final String _apiKey = "ImgBb_API_KEY";
 
   String get apiUrl => _apiUrl;
   String get apiKey => _apiKey;
